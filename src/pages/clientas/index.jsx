08b8168147891914c47935085b91290354cd2072
Eduardo Meza <joseduardomezacanul@hotmail.com>
2025-09-10 19:13:23 -0600

    Correccion de bug en descuentos. Se agrega comprobacion: si el total de citas estan pagadas y no estan canceladas si aplica descuento

--- src/pages/clientas/index.jsx
+++ src/pages/clientas/index.jsx
@@ -1,30 +1,148 @@
 import { useEffect, useState } from "react";
-import { ActionsClienta, SelectClientas } from "../nueva-cita/[date]";
-import { Box } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Grid,
+    Heading,
+    HStack,
+    Input,
+    Spinner,
+} from "@chakra-ui/react";
 import { loadHook } from "@/utils/lattice-design";
+import { AgGridReact } from "ag-grid-react";
+import "@/config/agGridSetup";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Clientas() {
-    const [clientasState, setClientasState] = useState("buscar");
-    const [currentPaso, setCurrentPaso] = useState("Lista");
-    const [loading, setLoading] = loadHook("useLoader")
+    const [loading, setLoading] = loadHook("useLoader");
+    const [clientas, setClientas] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [total, setTotal] = useState(null);
 
-    useEffect(()=>{
-        setLoading(false)
-    }, [])
+    useEffect(() => {
+        setLoading(true);
+        axios.get("/api/clientas").then((clientasResp) => {
+            console.log(clientasResp.data);
+            setClientas(clientasResp.data);
+            setTotal(clientasResp.data.length);
+            setLoading(false);
+        });
+    }, []);
+
+    const columnDefs = [
+        {
+            headerName: "Nombre Completo",
+            // field: "nombres",
+            valueGetter: (params) =>
+                `${params.data.nombres} ${params.data.apellidos}`,
+            flex: 3,
+            cellStyle: {
+                verticalAlign: "middle",
+            },
+        },
+        {
+            headerName: "Teléfono",
+            valueGetter: (params) =>
+                `+${params.data.lada} ${params.data.telefono}`,
+            flex: 2,
+            cellStyle: {
+                verticalAlign: "middle",
+            },
+        },
+        {
+            headerName: "Acciones",
+            cellRenderer: EditButton,
+            cellStyle: {
+                textAlign: "center",
+                marginTop: "0.5rem",
+                marginBottom: "0.5rem",
+                verticalAlign: "middle",
+            },
+            flex: 1,
+        },
+    ];
 
     return (
-        <Box py={"2rem"} w={"80%"}>
+        <Box
+            py={"2rem"}
+            w={"80%"}
+        >
             <ActionsClienta
-                clientasState={clientasState}
-                setClientasState={setClientasState}
-                currentPaso={currentPaso}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                total={total}
             />
-            <SelectClientas
-                clientasState={clientasState}
-                setClientasState={setClientasState}
-                currentPaso={currentPaso}
-                setCurrentPaso={setCurrentPaso}
-            />
+            <Box
+                w={"100%"}
+                h={"70vh"}
+                id={"AG-Table"}
+            >
+                {!clientas && (
+                    <Spinner
+                        color="pink.500"
+                        borderWidth="4px"
+                        size={"xl"}
+                    />
+                )}
+                {clientas && (
+                    <AgGridReact
+                        rowData={clientas}
+                        columnDefs={columnDefs}
+                        rowHeight={60}
+                        quickFilterText={searchTerm}
+                        headerbackgroundColor={"#434343"}
+                    />
+                )}
+            </Box>
         </Box>
+    );
+}
+
+function ActionsClienta({
+    searchTerm,
+    setSearchTerm,
+    total,
+}) {
+    return (
+        <HStack
+            w={"100%"}
+            justify={"space-between"}
+            mb={"1rem"}
+            pe={"0.5rem"}
+        >
+            <Input
+                w={"20rem"}
+                shadow={"md"}
+                bg={"white"}
+                size={"sm"}
+                placeholder="Buscar"
+                value={searchTerm}
+                onChange={(e) =>
+                    setSearchTerm(e.target.value)
+                }
+            />
+            {total && (
+                <Heading size={"md"}>
+                    Total: {total}
+                </Heading>
+            )}
+        </HStack>
+    );
+}
+
+function EditButton({ data }) {
+    const [loading, setLoading] = loadHook("useLoader");
+    const router = useRouter();
+    return (
+        <Button
+            onClick={() => {
+                setLoading(true);
+                router.push(`/clientas/${data.id}`);
+            }}
+            bg={"pink.500"}
+        >
+            Editar
+        </Button>
     );
 }
