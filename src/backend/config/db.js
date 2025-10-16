9e83e3fea8103e8cff5bb862a7f437af7b59af2a
Eduardo Meza <joseduardomezacanul@hotmail.com>
2025-10-15 23:33:33 -0600

    Remaking env vars

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -1,18 +1,5 @@
 export const dbConfig = {
-    // LOCAL
-    // host: "localhost",
-    // port: 3306,
-    // user: "root",
-    // password: "",
-    // database: "hadassa",
-
     // REMOTE
-    // host: "162.241.60.214",
-    // port: 3306,
-    // user: "joseedu4_administrador",
-    // password: "Eduardo_26_$",
-    // database: "joseedu4_hadassa",
-
     host: process.env.REMOTE_HOST,
     port: process.env.REMOTE_PORT,
     user: process.env.REMOTE_USER,
