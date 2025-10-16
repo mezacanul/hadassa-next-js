609dbb56390e9524a2409c192ca8f54dbbf54fe1
Eduardo Meza <joseduardomezacanul@hotmail.com>
2025-10-15 23:21:34 -0600

    Setup de variables de entorno

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -1,15 +1,21 @@
 export const dbConfig = {
-    // LOCAL 
+    // LOCAL
     // host: "localhost",
     // port: 3306,
     // user: "root",
     // password: "",
     // database: "hadassa",
-    
-    // REMOTE 
-    host: "162.241.60.214",
-    port: 3306,
-    user: "joseedu4_administrador",
-    password: "Eduardo_26_$",
-    database: "joseedu4_hadassa",
+
+    // REMOTE
+    // host: "162.241.60.214",
+    // port: 3306,
+    // user: "joseedu4_administrador",
+    // password: "Eduardo_26_$",
+    // database: "joseedu4_hadassa",
+
+    host: process.env.REMOTE_HOST,
+    port: process.env.REMOTE_PORT,
+    user: process.env.REMOTE_USER,
+    password: process.env.REMOTE_PSW,
+    database: process.env.REMOTE_DB,
 };
